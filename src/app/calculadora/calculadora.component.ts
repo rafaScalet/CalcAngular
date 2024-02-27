eb52c7044c10d707c06ae4c4ed3e5e9a465f30c6
rafaScalet <rafaelscalet2018@gmail.com>
2024-02-27 17:24:48 -0300

    created the calculadora component

---
+++ src/app/calculadora/calculadora.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-calculadora',
+  templateUrl: './calculadora.component.html',
+  styleUrl: './calculadora.component.css'
+})
+export class CalculadoraComponent {
+  num1: number = 0
+  num2: number = 0
+  result: number = 0
+}
